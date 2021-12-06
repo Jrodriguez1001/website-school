93ce18eaca0505f58e3311b7803e781477141f19
Jean Piere <jrodriguez100196@gmail.com>
2021-12-06 09:34:00 -0500

    Iniciamos el proyecto

---
+++ src/App.svelte
@@ -0,0 +1,69 @@
+<script>
+import Header from "./components/Header.svelte";
+export let name;
+
+
+</script>
+
+<main>
+<Header></Header>
+	<section class="header">
+		<div class="header-elements">
+			<div>logo</div>
+			<div>menu</div>
+		</div>
+	</section>
+	<section class="background">
+		<div class="background-elements">
+			<div class="divisor">
+				<div>
+					<h1>{name}</h1>
+					<button>Saber Más</button>
+				</div>
+			</div>
+			<div>
+			<img src="./img/sin-amor-no-hay-aprendizaje.jpg" alt="imagen_1">
+			</div>
+			
+		</div>
+	</section>
+	
+</main>
+
+<style>
+:global(body){
+color:#424242;
+}
+
+:global(:root){
+	--theme-color-main:#470205;
+	--theme-color-secondary:#424242;
+}
+
+
+.divisor{
+	display: flex;
+	flex-direction: row;
+	justify-content: center;
+}
+
+.header-elements{
+	display: flex;
+	flex-direction: row;
+	justify-content: space-around;
+	background-color:#E5FCF5;
+	padding: 20px 0px 20px 0px;
+}
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ src/components/Header.svelte
@@ -0,0 +1,69 @@
+<script>
+import Email from "./icons/Email.svelte";
+import Phone from "./icons/Phone.svelte";
+
+</script>
+<style>
+.menu-header{
+      display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    align-items: center;
+
+}
+.contactos{
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+}
+.contacto{
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    margin-right: 20px
+}
+
+.menu{
+    display: flex;
+    flex-direction: row;
+}
+ul {
+  list-style-type: none;
+}
+.icon{
+    margin: 0px 5px 0px 5px;
+}
+li{
+    margin: 0px 10px 0px 10px;
+}
+</style>
+
+<section class="Header">
+    <div class="menu-header">
+        <div class= "contactos">
+            <div class="contacto">
+                <div  class="icon"><Email></Email></div>
+                <div>tramites@ievs.edu.pe</div>
+            </div>
+            <div  class="contacto">
+                <div  class="icon"><Phone></Phone> </div>
+                <div>+51992964415 </div>
+            </div>
+        </div>
+        <nav >
+            <ul class="menu">
+                <li>Login</li>
+                <li>Registro</li>
+                <li>IconFB</li>
+                <li>IconInsta</li>
+                <li>IconYt</li>
+            </ul>    
+        </nav>
+    </div>
+    <div>
+        <div>logo</div>
+        <button>Donar Ahora</button>
+        <div>menu</div>
+    </div>
+
+</section>
